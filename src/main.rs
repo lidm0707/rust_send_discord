0b4a0326135de938c66a052c6315420982b8ae93
lilidm <lightdemon17@gmail.com>
2025-03-27 17:20:31 +0700

    change get time

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,15 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
     let webhook_url = env::var("URL")?;
 
-    let time = Local::now().format("%Y-%m-%d %H:%M:%S");
-
-    let payload = serde_json::json!({
-        "content": format!("Hello from home at {}", time),
-    });
-
     let client = Client::new();
     info!("Application started");
     loop {
         info!("loop");
+        let time = Local::now().format("%Y-%m-%d %H:%M:%S");
+
+        let payload = serde_json::json!({
+            "content": format!("Hello from home at {}", time),
+        });
         let response = client.post(&webhook_url).json(&payload).send().await?;
 
         if response.status().is_success() {
