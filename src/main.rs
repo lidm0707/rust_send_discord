8859be5d33e53d6ecef35e5ba8ef2dda632a9c9a
lilidm <lightdemon17@gmail.com>
2025-05-06 11:15:16 +0700

    🛑 Fixed crashing issue

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use dotenvy::dotenv;
 use reqwest::Client;
 use std::env;
 use tokio::time::{Duration, sleep};
-use tracing::{Level, info}; 
+use tracing::{error, info, warn, Level}; 
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -24,9 +24,11 @@ async fn main() -> Result<()> {
         let response = client.post(&webhook_url).json(&payload).send().await?;
 
         if response.status().is_success() {
-            println!("Message sent successfully!");
-        } else {
-            eprintln!("Failed to send message: {}", response.status());
+            info!("Message sent successfully!");
+        } else if response.status().is_server_error(){
+            warn!("Server error: {}", response.status());
+        }else {
+            error!("Failed to send message: {}", response.status());
         }
         // Sleep for 8 hours
         sleep(Duration::from_secs(8 * 60 * 60)).await;
